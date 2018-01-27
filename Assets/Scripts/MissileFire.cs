3a6ab2b37072ec9c86aee47613974c2b104c3348
Anthony Huber-Winkler <ranthonywinkler@gmail.com>
2018-01-27 13:54:57 -0600

    Fixed Missile
    I GOT IT TO WORK

--- Assets/Scripts/MissileFire.cs
+++ Assets/Scripts/MissileFire.cs
@@ -3,22 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MissileFire : MonoBehaviour {
+    
+    [SerializeField]
+    private Vector3 startMarker;
+    [SerializeField]
+    private Vector3 endMarker;
+    [SerializeField]
+    private float time = 5F;
+    private float startTime;
+    private float journeyLength;
 
-    float speed = 5;
-    float rotation;
+    void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker, endMarker);
+    }
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) / time;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+    }
 
+    void SetMissile (Vector3 startVal, Vector3 endVal)
+    {
+        startMarker = startVal;
+        endMarker = endVal;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker, endMarker);
+    }
 
-
-    // Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+   void SetTime (float stageTime)
+    {
+        time = stageTime;
+    }
 
 
 
