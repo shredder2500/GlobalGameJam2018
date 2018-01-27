83766ee9e37232208249e85d51725ef69d4a1cbf
Chris Taylor <shredder.ct@gmail.com>
2018-01-27 00:30:44 -0600

    queue angle commands

--- Assets/Scripts/CannonControl.cs
+++ Assets/Scripts/CannonControl.cs
@@ -9,8 +9,11 @@ public class CannonControl : MonoBehaviour
     private float _rotateSpeed = 5;
     private AnimationCurve _fireAngleCurve = 
         new AnimationCurve(new Keyframe(0, 90), new Keyframe(.5f, 0), new Keyframe(1, -90));
-
+    
     private float _fireAngle = 0;
+    private bool _targetLocked = false;
+
+    public event Action TargetLocked;
     
     private Quaternion CalcTargetQuaternion(Vector3 rotation) =>
         Quaternion.Euler(rotation.x, rotation.y, Mathf.Clamp(_fireAngle, -_maxAngle, _maxAngle));
@@ -18,8 +21,21 @@ public class CannonControl : MonoBehaviour
     private Quaternion CalcTargetQuaternion() =>
         CalcTargetQuaternion(transform.rotation.eulerAngles);
 
-    private void Update() =>
-        transform.rotation = Quaternion.Lerp(transform.rotation, CalcTargetQuaternion(), _rotateSpeed * Time.deltaTime);
+    private void Update()
+    {
+        if (!CloseEnough(transform.rotation.eulerAngles.z, _fireAngle))
+        {
+            _targetLocked = false;
+            transform.rotation = Quaternion.Lerp(transform.rotation, CalcTargetQuaternion(), _rotateSpeed * Time.deltaTime);
+        }
+        else if(!_targetLocked)
+        {
+            _targetLocked = true;
+            TargetLocked?.Invoke();
+        }
+    }
+
+    private bool CloseEnough(float a, float b, float value = .5f) => Math.Abs(a - b) <= value;
 
     public void SetFireAngle(float angle) => _fireAngle = _fireAngleCurve.Evaluate(angle / 180);
 }
--- Assets/Scripts/MissionControl.cs
+++ Assets/Scripts/MissionControl.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MissionControl : MonoBehaviour
@@ -10,11 +11,35 @@ public class MissionControl : MonoBehaviour
     private Queue<float> _cannonCommands
         = new Queue<float>();
 
+    private bool _isExecuting = false;
+
     public void QueueFireAngle(float angle)
-        => _cannonCommands.Enqueue(angle);
+    {
+        if (_isExecuting) return;
+        _cannonCommands.Enqueue(angle);
+        Debug.Log($"Queuing angle command {angle}");
+    }
+    
+    private void NextCommand()
+    {
+        if(_cannonCommands.Any())
+        {
+            Debug.Log($"Setting cannon angle to {_cannonCommands.Peek()}");
+            _cannon.SetFireAngle(_cannonCommands.Dequeue());
+        }
+        else
+        {
+            _cannon.TargetLocked -= NextCommand;
+            _isExecuting = false;
+        }
+    }
 
     public void Execute()
     {
+        if (_isExecuting) return;
 
+        _isExecuting = true;
+        _cannon.TargetLocked += NextCommand;
+        NextCommand();
     }
 }
